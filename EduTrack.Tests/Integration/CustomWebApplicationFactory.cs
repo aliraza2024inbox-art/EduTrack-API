8072fe4794a66f31ea139dff1d2490d7d34a785d
codewithali <aliraza2024inbox@gmail.com>
2026-07-30 17:18:52 +0500

    Complete Phase 12.2 - Integration test setup

---
+++ EduTrack.Tests/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,40 @@
+using EduTrack.Api.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EduTrack.Tests.Integration;
+
+public class CustomWebApplicationFactory
+    : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<AppDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("EduTrackTestDb");
+            });
+
+            var provider = services.BuildServiceProvider();
+
+            using var scope = provider.CreateScope();
+
+            var db = scope.ServiceProvider
+                .GetRequiredService<AppDbContext>();
+
+            db.Database.EnsureCreated();
+        });
+    }
+}
--- EduTrack/Program.cs
+++ EduTrack/Program.cs
@@ -162,3 +162,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+public partial class Program
+{
+}
