05b7225730ddbd20b8793413e7d46a16295f630a
codewithali <aliraza2024inbox@gmail.com>
2026-07-31 22:23:15 +0500

    Apply code formatting

--- EduTrack.Tests/Integration/HealthCheckTests.cs
+++ EduTrack.Tests/Integration/HealthCheckTests.cs
@@ -6,20 +6,20 @@ namespace EduTrack.Tests.Integration;
 
 public class HealthCheckTests : IClassFixture<CustomWebApplicationFactory>
 {
-	private readonly HttpClient _client;
+    private readonly HttpClient _client;
 
-	public HealthCheckTests(CustomWebApplicationFactory factory)
-	{
-		_client = factory.CreateClient();
-	}
+    public HealthCheckTests(CustomWebApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
 
-	[Fact]
-	public async Task Swagger_ShouldBeAvailable()
-	{
-		// Act
-		var response = await _client.GetAsync("/swagger/index.html");
+    [Fact]
+    public async Task Swagger_ShouldBeAvailable()
+    {
+        // Act
+        var response = await _client.GetAsync("/swagger/index.html");
 
-		// Assert
-		response.StatusCode.Should().Be(HttpStatusCode.OK);
-	}
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
 }
